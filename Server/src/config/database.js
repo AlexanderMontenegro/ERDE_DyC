685860eaaaafb1bf4ed5bb93b790344791938531
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-08-22 22:27:49 -0300

    22.27

--- Server/src/config/database.js
+++ Server/src/config/database.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+// Determinar las variables de entorno según el entorno
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  isProduction ? process.env.DB_NAME_R : process.env.DB_NAME,
+  isProduction ? process.env.DB_USER_R : process.env.DB_USER,
+  isProduction ? process.env.DB_PASSWORD_R : process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: isProduction ? process.env.DB_HOST_R : process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: isProduction ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Este es necesario para conexiones SSL en algunos entornos de Render
+      }
+    } : {},
   }
 );
 
