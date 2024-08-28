5f46ce6a2415b53cff60faa3eccf74de4df6d4b9
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-08-28 03:05:52 -0300

    28/08-03.05

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,7 +9,7 @@ import Administracion from './pages/Administracion';
 import Servicios from './pages/Servicios';
 import Estampado_c from './components/Estampado_c';
 import Personalizacion_c from './components/Personalizacion_c';
-import Impresion3D_c from './components/Impresion3D_c';
+import Impresion3d_c from './components/Impresion3D_c';
 
 
 function App() {
@@ -25,7 +25,7 @@ function App() {
         <Route path='/servicios' element={<Servicios />} />
         <Route path='/estampado_c' element={<Estampado_c/>} />
         <Route path='/personalizacion_c' element={<Personalizacion_c/>}/>
-        <Route path='/impresion3d_c' element={ <Impresion3D_c/>}/>
+        <Route path='/impresion3d_c' element={ <Impresion3d_c/>}/>
         
       </Routes>
     </Router>
